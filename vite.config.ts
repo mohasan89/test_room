c2287d4d7eba8ff85ca443c4d883f3ee98149cbc
Mohamad <mo.hasan89@gmail.c om>
2024-12-23 13:24:48 +0300

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/test_room/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
